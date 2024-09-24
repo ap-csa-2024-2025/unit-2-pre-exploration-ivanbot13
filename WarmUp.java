d54103ab970495ef67bc21c6759c335071e23ccb
ivanbot13 <ikduong100@student.hbuhsd.edu>
2024-09-24 10:46:24 -0700

    warmup

--- WarmUp.java
+++ WarmUp.java
@@ -2,10 +2,19 @@ public class WarmUp
 {
   public static void main(String[] args)
   {
-    // test your methods here
+    System.out.println(isEven(9));
+    System.out.println(isDiv(25, 5));
   }
 
   // write your methods below
+  public static boolean isEven(int a)
+  {
+    return(a % 2 == 0);
+  }
 
-  
+  public static boolean isDiv(int a, int b)
+  {
+    return(a % b == 0);
+  }
+
 }
